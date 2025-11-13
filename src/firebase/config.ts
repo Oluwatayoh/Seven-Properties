388fca42cfd214552d7524c0bb147a30bcb07015
Ojo Oluwatayo Michael <ojotayoh2@gmail.com>
2025-11-13 21:53:49 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,2 +1,10 @@
 // This file is intentionally left blank.
 // The firebase config will be populated by the backend.
+export const firebaseConfig = {
+  "projectId": "studio-7957115073-fe541",
+  "appId": "1:1008660453238:web:3072f14c86df51aabecc57",
+  "apiKey": "AIzaSyD_-AmP_W01OPD4BnHT9cVkcsCw6BTIiP4",
+  "authDomain": "studio-7957115073-fe541.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1008660453238"
+};
