1d90cba8bce03dd231dcd10b141ef192f1b906f3
Oluwatayo <nsmartware.oluwatayo@equitybank.co.ke>
2026-05-28 20:54:10 +0100

    claude branch update

--- src/firebase/config.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  "projectId": "studio-7957115073-fe541",
-  "appId": "1:1008660453238:web:3072f14c86df51aabecc57",
-  "apiKey": "AIzaSyD_-AmP_W01OPD4BnHT9cVkcsCw6BTIiP4",
-  "authDomain": "studio-7957115073-fe541.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "1008660453238"
-};
